46628ebc2f8a781263ae021e17581cc049dd9fd9
MohamedMokhtar98 <74834205+MohamedMokhtar98@users.noreply.github.com>
2023-02-03 16:02:23 +0200

    Adding ADC

---
+++ 02-MCAL/02-ADC/ADC_Interface.h
@@ -0,0 +1,60 @@
+/*
+ * ADC_Interface.h
+ *
+ *  Created on: Feb 03, 2023
+ *      Author: Mohamed Mokhtar
+ */
+#ifndef ADC_INTERFACE_H_
+#define ADC_INTERFACE_H_
+
+#include "STD_TYPES.h"
+
+
+/************************ Private Section *********************************/
+#define AREF_PIN    0
+#define VCC_5V      1
+#define RESERVED    2
+#define INTERNAL_2  3
+#define x2    0x01
+#define x4    0x02
+#define x8    0x03
+#define x16   0x04
+#define x32   0x05
+#define x64   0x06
+#define x128  0x07
+/**************************************************************************/
+
+/******************* ProtoTypes Global Function Section *******************/
+void ADC_Init(u8 Channel);
+u16 ADC_Read(u8 Channel);
+/**************************************************************************/
+
+
+/************************* Configuration Section **************************/
+/*
+Reference voltage selection for ADC:
+Option:
+    AREF_PIN
+    VCC_5V
+    RESERVED
+    INTERNAL_2
+*/
+#define REFERENCE_VOLTAGE_SELECTION VCC_5V
+
+/*
+ADC Prescaler:
+Option:
+       x2
+       x4
+       x8
+       x16
+       x32
+       x64
+       x128
+*/
+#define ADC_PRESCALER x128
+
+/**************************************************************************/
+
+
+#endif /* ADC_HEADER_H_ */
